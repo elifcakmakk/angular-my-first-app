2c8ff8387a1c983a95ed3da791d799764388c55c
elifcakmakk <elif.cakmak@outlook.com.tr>
2021-03-29 11:53:32 +0300

    second componenet added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-//import {FormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
+import {ServerComponenet} from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponenet,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
-    //FormsModule
+    FormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
